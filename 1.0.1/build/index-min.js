e01a7e94e230c1c08c43c88f7d8b0487601ae24a
kissyteam <tiehang.lth@taobao.com>
2015-01-16 15:59:30 +0800

    sync daily branch 1.0.1

--- 1.0.1/build/index-min.js
+++
@@ -1,1 +0,0 @@
-KISSY.add("kg/new-tab/1.0.0/index",["dom","event"],function(e,t,n,a){var r,o=t("dom"),i=t("event");r=function(e){var t=o,n=i,a={"goto":function(e,a){if(1==a){var r=t.create('<form id="openWin" action="'+e+'" target="_blank" method="get"></form>');r&&r.submit()}else{var o=t.create('<a target="_blank" href="'+e+'"></a>');n.fire(o,"click")}}};return e=a}(),a.exports=r});
--- guide/index.md
+++ guide/index.md
@@ -5,7 +5,7 @@ NewTab。
 ## 初始化组件
 		
     S.use('kg/new-tab/1.0.0/index', function (S, NewTab) {
-         var new-tab = new NewTab();
+         var newTab = new NewTab();
     })
 
 ## API说明
